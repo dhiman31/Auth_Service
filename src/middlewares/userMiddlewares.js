97f71461d763af8f6334ee01a54a231c4ab136eb
dhiman31 <ankushiiituna@gmail.com>
2025-12-30 18:21:30 +0530

    Added AuthValidate Middleware

--- src/middlewares/userMiddlewares.js
+++ src/middlewares/userMiddlewares.js
@@ -1,33 +1,4 @@
-const validLoginRequest = (req,res,next) => {
-    const {email , password} = req.body;
-    if(!email || email.trim() === '')
-    {
-        return res.status(400).json({
-        success: false,
-        message: 'Email is required',
-        data: {},
-        err: {}
-        });
-    }
-    if(!password || password.trim() === '')
-    {
-        return res.status(400).json({
-        success: false,
-        message: 'Password is required',
-        data: {},
-        err: {}
-    });
-    }
-
-    req.body = {
-    email: email.trim(),
-    password: password
-    };
-
-    next();
-}
-
-const validSignUpRequest = (req,res,next) => {
+const validAuthRequest = (req,res,next) => {
     const {email , password} = req.body;
     if(!email || email.trim() === '')
     {
@@ -57,6 +28,5 @@ const validSignUpRequest = (req,res,next) => {
 }
 
 module.exports = {
-    validLoginRequest,
-    validSignUpRequest
+    validAuthRequest
 }
--- src/routes/v1/index.js
+++ src/routes/v1/index.js
@@ -1,10 +1,10 @@
 const user_controller = require('../../controllers/userController')
 const express = require('express');
 const router = express.Router();
-const {validLoginRequest , validSignUpRequest} = require('../../middlewares/userMiddlewares');
+const {validAuthRequest} = require('../../middlewares/userMiddlewares');
 
-router.post('/user/signup',validSignUpRequest,user_controller.Register)
-router.post('/user/login',validLoginRequest,user_controller.login)
+router.post('/user/signup',validAuthRequest,user_controller.Register)
+router.post('/user/login',validAuthRequest,user_controller.login)
 router.delete('/user/delete',user_controller.deleteAccount)
 
 module.exports = router;
